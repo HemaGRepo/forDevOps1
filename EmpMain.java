c0789fcb27b57ea1fe61b5d9842269fe76e4c2d0
G <hema.g@capgemini.com>
2018-11-27 15:20:22 +0530

    V3.0

--- EmpMain.java
+++ EmpMain.java
@@ -2,6 +2,10 @@
 public class EmpMain {
 
 	public static void main(String[] args) {
+	 int i=0;
+	 for(i=0;i<=10;i++)
+	  System.out.println("The loop executes......"+i);
+	  System.out.println("_________________");
 		System.out.println("Hello.");
 		System.out.println("Welcome to Java");
 		System.out.println("_________________");
