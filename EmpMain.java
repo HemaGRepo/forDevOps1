01b36f2aa2e65a42e18bf7dd9d0116ba927db363
G <hema.g@capgemini.com>
2018-11-27 14:41:17 +0530

    V2.0

---
+++ EmpMain.java
@@ -0,0 +1,10 @@
+
+public class EmpMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello.");
+		System.out.println("Welcome to Java");
+		System.out.println("_________________");
+	}
+
+}
